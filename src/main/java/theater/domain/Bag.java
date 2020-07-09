3f00858e94d5ba7164956cd0af8890ba64a5e5eb
김다혜 <dahye432111@gmail.com>
2020-07-09 22:09:00 +0900

    feat: 1-02 설계 개선하기
    코드의 자율성을 높이기 위한 코드 개선

--- src/main/java/theater/domain/Bag.java
+++ src/main/java/theater/domain/Bag.java
@@ -14,15 +14,11 @@ public class Bag {
         this.invitation = invitation;
     }
 
-    public boolean hasInvitation() {
+    private boolean hasInvitation() {
         return invitation != null;
     }
 
-    public boolean hasTicket() {
-        return ticket != null;
-    }
-
-    public void setTicket(Ticket ticket) {
+    private void setTicket(Ticket ticket) {
         this.ticket = ticket;
     }
 
@@ -30,7 +26,14 @@ public class Bag {
         this.amount -= amount;
     }
 
-    public void plusAmount(Long amount) {
-        this.amount += amount;
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(amount);
+            return ticket.getFee();
+        }
     }
 }
