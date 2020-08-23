a23d6a364e64d9a9b07e272913eab07ae8b1e5a1
김다혜 <dahye432111@gmail.com>
2020-08-23 22:26:22 +0900

    feat: 상속과 중복 코드 - 상속을 이용해서 중복코드 제거하기

--- src/main/java/chapter10/NightlyDiscountPhone.java
+++ src/main/java/chapter10/NightlyDiscountPhone.java
@@ -3,34 +3,30 @@ package chapter10;
 import money.Money;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
-public class NightlyDiscountPhone {
+public class NightlyDiscountPhone extends Phone {
     private static final int LATE_NIGHT_HOUR = 22;
 
     private Money nightlyAmount;
-    private Money regularAmount;
-    private Duration seconds;
-    private List<Call> calls = new ArrayList<>();
 
     public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+        super(regularAmount, seconds);
         this.nightlyAmount = nightlyAmount;
-        this.regularAmount = regularAmount;
-        this.seconds = seconds;
     }
 
+    @Override
     public Money calculateFee() {
-        Money result = Money.ZERO;
+        Money result = super.calculateFee();
 
-        for (Call call : calls) {
+        Money nightlyFee = Money.ZERO;
+        for (Call call : getCalls()) {
             if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
-                result = result.plus(nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
-            } else {
-                result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+                nightlyFee = nightlyFee.plus(
+                        getAmount().minus(nightlyAmount).times(
+                                call.getDuration().getSeconds() / getSeconds().getSeconds()));
             }
         }
-        
-        return result;
+
+        return result.minus(nightlyFee);
     }
 }
