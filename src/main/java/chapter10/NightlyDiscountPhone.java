0008e1e8db48f06bdea7ad0c9e5e5ea29696a8ed
김다혜 <dahye432111@gmail.com>
2020-08-23 23:29:56 +0900

    feat: Phone 다시 살펴보기 - 추상화에 의존하자 (코드 개선을 위해 상속 이전의 상태로 복원)

--- src/main/java/chapter10/NightlyDiscountPhone.java
+++ src/main/java/chapter10/NightlyDiscountPhone.java
@@ -3,30 +3,34 @@ package chapter10;
 import money.Money;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
-public class NightlyDiscountPhone extends Phone {
+public class NightlyDiscountPhone {
     private static final int LATE_NIGHT_HOUR = 22;
 
     private Money nightlyAmount;
+    private Money regularAmount;
+    private Duration seconds;
+    private List<Call> calls = new ArrayList<>();
 
     public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
-        super(regularAmount, seconds);
         this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
     }
 
-    @Override
     public Money calculateFee() {
-        Money result = super.calculateFee();
+        Money result = Money.ZERO;
 
-        Money nightlyFee = Money.ZERO;
-        for (Call call : getCalls()) {
+        for (Call call : calls) {
             if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
-                nightlyFee = nightlyFee.plus(
-                        getAmount().minus(nightlyAmount).times(
-                                call.getDuration().getSeconds() / getSeconds().getSeconds()));
+                result = result.plus(nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            } else {
+                result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
             }
         }
 
-        return result.minus(nightlyFee);
+        return result;
     }
 }
